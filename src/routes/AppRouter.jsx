a3249766ffdd7ad79e68fa293fe9ea9245ca5165
carlOzil <seifer01es@msn.com>
2023-10-12 22:11:11 +0200

    merge try

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,40 +3,37 @@ import { useEffect, useState } from "react";
 import { AuthRouter } from "./AuthRouter";
 import { RealestateRouter } from "./RealestateRouter";
 import { ContactPage, AboutUsPage } from "../realestate/pages/index";
-// Importacion de herramientas de autenticacion de Firebase
-import { auth } from "../auth/config/firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
+//get login status from firebase:
+//import { auth } from "../config/firebaseConfig";
+//import { onAuthStateChanged } from "firebase/auth";
 
 export const AppRouter = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  // Comprueba si el usuario está autenticado y cambia el estado según el caso
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        setIsAuthenticated(true);
-      }
-    });
-  }, []);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const isAuthenticated = false;
+  //check if user is logged and if so, set isAuthenticated to "true"
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, async (user) => {
+  //     if (user) {
+  //       setIsAuthenticated(true);
+  //     }
+  //   });
+  // }, []);
 
   return (
     <>
       {/* <HeaderComp /> */}
-     
-        
+
       <div className="bg-stone-300 min-h-screen">
-      <Routes>
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/about" element={<AboutUsPage />} />
-        {!isAuthenticated ? (
-          <Route path="/*" element={<AuthRouter />}></Route>
-        ) : (
-          <Route path="/*" element={<RealestateRouter />}></Route>
-        )}
-      </Routes>
-
+        <Routes>
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          {!isAuthenticated ? (
+            <Route path="/*" element={<AuthRouter />}></Route>
+          ) : (
+            <Route path="/*" element={<RealestateRouter />}></Route>
+          )}
+        </Routes>
       </div>
-
-     
     </>
   );
 };
