a499aef46dbca59939aca1b00e1d682a6432adad
dorian-rose <dorianrkelly@gmail.com>
2023-10-12 12:34:57 +0200

    pre login home page route created

--- src/routes/AuthRouter.jsx
+++ src/routes/AuthRouter.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { LoginPage, RegisterPage } from "../auth/pages/index";
+import { LoginPage, RegisterPage, StartPage } from "../auth/pages/index";
 export const AuthRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<StartPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/*" element={<Navigate to={"/"} />} />
     </Routes>
